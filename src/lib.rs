9a4fb06090995774457418da32666e737f9a8d4d
rustaceanrob <rob.netzke@gmail.com>
2026-02-15 15:11:33 +0000

    Move L to member function

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
 use std::io::Read;
 
+/// An encoding scheme for representing a list of monotonically increasing elements.
 #[derive(Debug)]
 pub struct EliasFano {
     n: u16,
     m: u16,
-    low_bits: u8,
     low: Vec<u8>,
     high: Vec<u8>,
 }
 
 impl EliasFano {
+    /// Compress a unique, ordered set of elements.
     pub fn compress(elements: &[u16]) -> Self {
         debug_assert!(elements.is_sorted());
         debug_assert!(elements.len() < u16::MAX as usize);
@@ -17,60 +18,16 @@ impl EliasFano {
             return Self {
                 n: 0,
                 m: 0,
-                low_bits: 0,
                 low: Vec::new(),
                 high: Vec::new(),
             };
         }
         let m = *elements.last().expect("elements is non-empty.");
-        debug_assert!(m != u16::MAX);
         let n = elements.len() as u16;
-        let l = ((m + 1) / n).ilog2() as u8;
+        let l = Self::l(m, n);
         let low = Self::pack_low_bits(elements, l);
         let high = Self::unary_encode_high_bits(elements, l);
-        Self {
-            n,
-            m,
-            low_bits: l,
-            low,
-            high,
-        }
-        // let total_lower_bytes = (n as u32 * l as u32).div_ceil(8) as usize;
-    }
-
-    pub fn from_reader<R: Read>(reader: &mut R) -> Result<Self, std::io::Error> {
-        let mut n_buf = [0u8; 2];
-        reader.read_exact(&mut n_buf)?;
-        let n = u16::from_le_bytes(n_buf);
-        if n == 0 {
-            return Ok(Self {
-                n,
-                m: 0x00,
-                low_bits: 0x00,
-                low: Vec::new(),
-                high: Vec::new(),
-            });
-        }
-        let mut m_buf = [0u8; 2];
-        reader.read_exact(&mut m_buf)?;
-        let m = u16::from_le_bytes(m_buf);
-        let mut l_buf = [0u8; 1];
-        reader.read_exact(&mut l_buf)?;
-        let l = u8::from_le_bytes(l_buf);
-        let low_bytes = (n as usize * l as usize).div_ceil(8);
-        let mut low_buf = vec![0u8; low_bytes];
-        reader.read_exact(&mut low_buf)?;
-        debug_assert!(m != u16::MAX);
-        let high_bytes = (n as usize + (m >> l) as usize + 1).div_ceil(8);
-        let mut high_buf = vec![0u8; high_bytes];
-        reader.read_exact(&mut high_buf)?;
-        Ok(Self {
-            n,
-            m,
-            low_bits: l,
-            low: low_buf,
-            high: high_buf,
-        })
+        Self { n, m, low, high }
     }
 
     #[inline]
@@ -134,8 +91,49 @@ impl EliasFano {
         high
     }
 
+    /// Size of the representation in bytes.
     #[inline]
     pub fn size(&self) -> usize {
-        size_of::<u16>() + size_of::<u16>() + size_of::<u8>() + self.low.len() + self.high.len()
+        size_of::<u16>() + size_of::<u16>() + self.low.len() + self.high.len()
+    }
+
+    /// The number of lower bits in the representation, floor(log_2((`m` + 1)/ `n`)) where `n` is
+    /// the size of the list and `m` is the largest element.
+    #[inline]
+    const fn l(m: u16, n: u16) -> u8 {
+        debug_assert!(m != u16::MAX);
+        ((m + 1) / n).ilog2() as u8
+    }
+
+    /// Read a list representation from disk.
+    pub fn from_reader<R: Read>(reader: &mut R) -> Result<Self, std::io::Error> {
+        let mut n_buf = [0u8; 2];
+        reader.read_exact(&mut n_buf)?;
+        let n = u16::from_le_bytes(n_buf);
+        if n == 0 {
+            return Ok(Self {
+                n,
+                m: 0x00,
+                low: Vec::new(),
+                high: Vec::new(),
+            });
+        }
+        let mut m_buf = [0u8; 2];
+        reader.read_exact(&mut m_buf)?;
+        let m = u16::from_le_bytes(m_buf);
+        let l = Self::l(m, n);
+        let low_bytes = (n as usize * l as usize).div_ceil(8);
+        let mut low_buf = vec![0u8; low_bytes];
+        reader.read_exact(&mut low_buf)?;
+        debug_assert!(m != u16::MAX);
+        let high_bytes = (n as usize + (m >> l) as usize + 1).div_ceil(8);
+        let mut high_buf = vec![0u8; high_bytes];
+        reader.read_exact(&mut high_buf)?;
+        Ok(Self {
+            n,
+            m,
+            low: low_buf,
+            high: high_buf,
+        })
     }
 }
